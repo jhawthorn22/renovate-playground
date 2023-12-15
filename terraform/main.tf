052d51bf99b78831bdbb92966422d33323559414
josh.hawthornthwaite@pollinate.co.uk <josh.hawthornthwaite@pollinate.co.uk>
2023-12-15 17:07:12 +0000

    Add some some code for renovate poc

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+module "key-vault" {
+  source  = "aztfm/key-vault/azurerm"
+  version = "1.0.0"
+  name = "renovate-playground-kv"
+  resource_group_name = "renovate-playground-kv-rg"
+  location = "uksouth"
+  sku_name = "standard"
+  tenant_id = "SOME_ID"
+}
